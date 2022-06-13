9c6c9d6a8640dd4575675968cb1352b43b80e19b
Seb-degraff <seb.degraff@gmail.com>
2022-06-13 14:36:11 +0200

    login form

--- messenger_app/urls.py
+++ messenger_app/urls.py
@@ -3,5 +3,7 @@ from messenger_app import views
 
 urlpatterns = [
     path('', views.main_view),
-    path('all-messages', views.get_all_messages)
+    path('all-messages', views.get_all_messages),
+    path('login', views.login),
+    path('logout', views.logout),
 ]
--- messenger_app/views.py
+++ messenger_app/views.py
@@ -1,11 +1,14 @@
 from bdb import effective
+from distutils.log import error
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 #from django.views.decorators.csrf import csrf_exempt
 from .models import Message
+from django.contrib import auth
 
 def main_view(request):
+    print(request.user)
     sent_message_text = request.POST.get("message-text", "")
     if (sent_message_text != ""):
         new_message = Message.objects.create()
@@ -14,7 +17,11 @@ def main_view(request):
 
     messages = Message.objects.all()
 
-    return render(request, "messenger_app/main_page.html", { "send_message": sent_message_text, "messages": messages })
+    return render(request, "messenger_app/main_page.html", {
+        "send_message": sent_message_text,
+        "messages": messages,
+        "username": request.user.username,
+    })
 
 def get_all_messages(request):
     messages = Message.objects.all()
@@ -26,6 +33,31 @@ def get_all_messages(request):
     for message in messages:
         response["messages"].append(message.text)
 
-
-
     return JsonResponse(response)
+
+
+def login(request):
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    error_text = ""
+
+    if (username != "" and password != ""):
+        user = auth.authenticate(request, username=username, password=password)
+        if user is None:
+            error_text = "incorrect login credentials"
+        else:
+            auth.login(request, user)
+            return HttpResponseRedirect("/")
+
+    return render(request, "messenger_app/login_page.html", {
+        "error_text": error_text,
+        "username": request.user,
+    })
+    
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect("/")
+    
